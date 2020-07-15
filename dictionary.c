14cc4f8111d4d047c6799166a2014e05bf7dc1a4
ali77-eng <ali77-eng@users.noreply.github.com>
2020-07-15 00:12:32 +0000

    automated commit by check50 [check50=True]

--- dictionary.c
+++ dictionary.c
@@ -1,15 +1,14 @@
-#include <ctype.h>
 #include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <strings.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
 
 #include "dictionary.h"
 
-#define HASHTABLE_SIZE 10000
-
-// Defines struct for a node
+// Represents a node in a hash table
 typedef struct node
 {
     char word[LENGTH + 1];
@@ -17,113 +16,90 @@ typedef struct node
 }
 node;
 
-node *hashtable[HASHTABLE_SIZE];
 
-// Hashes the word (hash function posted on reddit by delipity)
-// The word you want to hash is contained within new node, arrow, word.
-// Hashing that will give you the index. Then you insert word into linked list.
-int hash_index(char *hash_this)
-{
-    unsigned int hash = 0;
-    for (int i = 0, n = strlen(hash_this); i < n; i++)
-    {
-        hash = (hash << 2) ^ hash_this[i];
-    }
-    return hash % HASHTABLE_SIZE;
-}
+// Number of buckets in hash table
+const unsigned int N = 676;
 
-// Initializes counter for words in dictionary
-int word_count = 0;
-
-// Loads dictionary into memory, returning true if successful else false
-bool load(const char *dictionary)
-{
-    // Opens dictionary
-    FILE *file = fopen(dictionary, "r");
-    if (file == NULL)
-    {
-        return false;
-    }
-    // Scans dictionary word by word (populating hash table with nodes containing words found in dictionary)
-    char word[LENGTH + 1];
-    while (fscanf(file, "%s", word) != EOF)
-    {
-        // Mallocs a node for each new word (i.e., creates node pointers)
-        node *new_node = malloc(sizeof(node));
-        // Checks if malloc succeeded, returns false if not
-        if (new_node == NULL)
-        {
-            unload();
-            return false;
-        }
-        // Copies word into node if malloc succeeds
-        strcpy(new_node->word, word);
-
-        // Initializes & calculates index of word for insertion into hashtable
-        int h = hash_index(new_node->word);
-
-        // Initializes head to point to hashtable index/bucket
-        node *head = hashtable[h];
-
-        // Inserts new nodes at beginning of lists
-        if (head == NULL)
-        {
-            hashtable[h] = new_node;
-            word_count++;
-        }
-        else
-        {
-            new_node->next = hashtable[h];
-            hashtable[h] = new_node;
-            word_count++;
-        }
-    }
-    fclose(file);
-    return true;
-}
+// Hash table
+node *table[N];
 
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
-    // Creates copy of word on which hash function can be performed
-    int n = strlen(word);
-    char word_copy[LENGTH + 1];
-    for (int i = 0; i < n; i++)
-    {
-        word_copy[i] = tolower(word[i]);
-    }
-    // Adds null terminator to end string
-    word_copy[n] = '\0';
-    // Initializes index for hashed word
-    int h = hash_index(word_copy);
-    // Sets cursor to point to same address as hashtable index/bucket
-    node *cursor = hashtable[h];
-    // Sets cursor to point to same location as head
+    // TODO
+    return false;
+}
 
-    // If the word exists, you should be able to find in dictionary data structure.
-    // Check for word by asking, which bucket would word be in? hashtable[hash(word)]
-    // While cursor does not point to NULL, search dictionary for word.
-    while (cursor != NULL)
+// Hashes word to a number
+unsigned int hash(const char *word)
+{
+    // TODO
+    int first = 0;
+    int second = 0;
+    if(word[0] > 64)
     {
-        // If strcasecmp returns true, then word has been found
-        if (strcasecmp(cursor->word, word_copy) == 0)
-        {
-            return true;
-        }
-        // Else word has not yet been found, advance cursor
-        else
-        {
-            cursor = cursor->next;
-        }
+        first = word[0]-32;
     }
-    // Cursor has reached end of list and word has not been found in dictionary so it must be misspelled
+    else
+    {
+        first = word[0];
+    }
+    if(word[1] > 64)
+    {
+        second = word[1]-32;
+    }
+    else
+    {
+        second = word[1];
+    }
+    int bucket = ((first - 65)*26) + (second - 65);
+    printf("%i", bucket);
+    return 0;
+}
+
+// Loads dictionary into memory, returning true if successful else false
+bool load(const char *dictionary)
+{
+    char * word = 0;
+    // TODO
+    //fopen
+    FILE * fp;
+    fp = fopen("dictionary", "w");
+    //check if return value is NULL
+    if (fp == NULL)
+    {
+        return 1;
+    }
+    //read strings
+    //fscanf(file, "%s", word)
+        //EOF means end of file
+    while (fscanf(fp, "%s", word) != EOF)
+    {
+        //create node
+        node *n = malloc(sizeof(node));
+        //check if return is NULL
+        if (n == NULL)
+        {
+            return false;
+        }
+        //copy word into node
+        strcpy(n->word, word);
+        n->next = NULL;
+        int bucket = hash(word);
+        n->next = table[bucket];
+        table[bucket] = n;
+        //use hash function to return an index
+        //insert word into linked list
+            //set pointers in correct order
+    }
     return false;
 }
 
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
-    return word_count;
+    // TODO
+    return 0;
 }
 
 // Unloads dictionary from memory, returning true if successful else false
